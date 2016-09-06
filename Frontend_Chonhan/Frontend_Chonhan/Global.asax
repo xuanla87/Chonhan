f997434c8ff30c805fbc3cb1fc46c2e0f199e7bb
xuanla87 <xuanla87@gmail.com>
2016-09-06 20:57:59 +0700

    Create

---
+++ Frontend_Chonhan/Frontend_Chonhan/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Frontend_Chonhan.MvcApplication" Language="C#" %>
---
+++ Frontend_Chonhan/Frontend_Chonhan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Frontend_Chonhan.Startup))]
+namespace Frontend_Chonhan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
